bf86fbeffc058a414ddd565b5eb7a6d464603d8f
grefano <gabrielnieed@gmail.com>
2025-10-01 00:04:35 -0300

    prettier submission ui

--- src/components/submission/Submission.tsx
+++ src/components/submission/Submission.tsx
@@ -4,10 +4,7 @@ import type { ISubmission } from "../../util/types";
 import CodeBlock from '../code_block/CodeBlock';
 
 export default function Submission({lang, code, owner}: ISubmission){
-    return (<div id='ctn-submission'><span className='font-p'>{owner}.{lang}</span> 
-        <CodeBlock language='python' code={code}/>
-            {/* <pre>
-                <code style={{'fontFamily': 'var(--font-code'}}>{(code).slice(0, 200)}</code>
-            </pre> */}
+    return (<div id='ctn-submission'><span id='submission-title' className='font-p'>{owner}.{lang}</span> 
+            <CodeBlock language='python' code={code}/>
         </div>)
 }
